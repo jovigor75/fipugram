49962a26bcacbf82b2163ec4c439b005517a638e
jovigor75 <jovigor@yahoo.com>
2022-11-01 13:55:02 +0100

    Nav Bar Završen

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,43 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/login">Login</router-link> |
-    <router-link to="/signup">Signup</router-link>
+  <nav id="nav" class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">
+      <img
+        src="@/assets/IgorGram.svg"
+        alt="Logo"
+        height="40"
+        class="d-inline-block align-top"
+        loading="lazy"
+      />
+    </a>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarToggler"
+      aria-controls="navbarToggler"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarToggler">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item"><router-link to="/">Home</router-link> |</li>
+        <li class="nav-item"><router-link to="/login">Login</router-link> |</li>
+        <li class="nav-item"><router-link to="/signup">Signup</router-link></li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input
+          class="form-control me-2"
+          type="search"
+          placeholder="Search"
+          aria-label="Search"
+        />
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
   </nav>
-  <router-view/>
+  <router-view />
 </template>
 
 <style lang="scss">
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,16 @@
 <template>
+    <div class="row">
+      <div class="col-2"></div>
+      <div class="col-7">... ovdje ide Instagram story!</div>
+      <div class="col-3">... ovdje ide desni stupac!</div>
+    </div>
+
+
+  
+  
+  
+  
+  
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App!!!"/>
@@ -7,12 +19,12 @@
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
