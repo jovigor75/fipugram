cbd414403ef3a507790e99f7198f03b568f8ff1e
jovigor75 <jovigor@yahoo.com>
2022-11-04 14:06:14 +0100

    Napravljena InstagramCard komponenta

---
+++ src/components/InstagramCard.vue
@@ -0,0 +1,29 @@
+<!-- Komponenta za prikazivanje instagram kartice-->
+
+<template>
+  <div class="card">
+    <!-- Bootstrap klasa card-->
+    <div class="card-header text-left">Featured</div>
+    <!--natpis u hederu poravnat u lijevo-->
+    <!--Tijelo card-->
+    <div class="card-body p-0">
+      <!-- Slučajna slika sa piscum-a širine 800-->
+      <img
+        class="card-img-top"
+        src="https://picsum.photos/800"
+        alt="Random image 1"
+      />
+    </div>
+    <div class="card-footer text-muted text-left">2 days ago</div>
+    <!--natpis u hederu poravnat u lijevo-->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "InstagramCard", // naziv komponente mora biti isti kao naziv fajla
+  props: {
+    msg: String,
+  },
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,30 +1,29 @@
 <template>
-    <div class="row">
-      <div class="col-2"></div>
-      <div class="col-7">... ovdje ide Instagram story!</div>
-      <div class="col-3">... ovdje ide desni stupac!</div>
+  <div class="row">
+    <div class="col-2"></div>
+    <div class="col-7">
+      <InstagramCard />
+      <InstagramCard />
+      <!--poziv komponente InstagramCard-->
     </div>
-
-
-  
-  
-  
-  
-  
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App!!!"/>
+    <div class="col-3">... ovdje ide Sidebar!</div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// @ is an alias to /src. Import komponente
+import InstagramCard from "@/components/InstagramCard.vue";
 
 export default {
-  name: "HomeView",
+  name: "HomeView", //naziv za view isti kao naziv fajla
   components: {
-    HelloWorld,
+    InstagramCard, //komponente koje se koriste u view-u
   },
 };
 </script>
+
+<style lang="scss">
+.card {
+  margin-bottom: 200 px;
+}
+</style>
