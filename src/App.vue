029583312927007aff7deeb46b13441b39593a80
jovigor75 <jovigor@yahoo.com>
2022-11-08 13:09:27 +0100

    ?

--- src/App.vue
+++ src/App.vue
@@ -40,6 +40,8 @@
   <router-view />
 </template>
 
+<script></script>
+
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
---
+++ src/store.js
@@ -0,0 +1,3 @@
+export default {
+    searchTerm : '' //Promjenjljiva kroz koja je dostupna u svim komponentama i u App.vue 
+}
